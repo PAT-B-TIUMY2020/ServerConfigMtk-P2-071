a0e2a359e58925d80c9229bd8dceb295f5c5d37e
AisyahDiniaKencana <56532105+AisyahDiniaKencana@users.noreply.github.com>
2020-10-17 15:10:29 +0700

    Add project files.

---
+++ ServerConfigMtk-P2-20180140071/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigMtk_P2_20180140071
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
