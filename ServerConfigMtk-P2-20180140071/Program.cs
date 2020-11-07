e0c20ef46c3fd004a20564c2b45d605dc170e22d
AisyahDiniaKencana <56532105+AisyahDiniaKencana@users.noreply.github.com>
2020-11-07 14:30:15 +0700

    add exception

--- ServerConfigMtk-P2-20180140071/Program.cs
+++ ServerConfigMtk-P2-20180140071/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMtk_P1_20180140071;
+
 
 namespace ServerConfigMtk_P2_20180140071
 {
@@ -10,6 +13,22 @@ namespace ServerConfigMtk_P2_20180140071
     {
         static void Main(string[] args)
         {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
         }
     }
 }
